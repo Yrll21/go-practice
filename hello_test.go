3eeb221f865b47d3268f701b83e9eacdbba1b0e3
Ivan Yrll Labadia <yrll@Ivans-MacBook-Air.local>
2024-09-02 19:08:13 +0800

    Refactor code and add french language

--- hello_test.go
+++ hello_test.go
@@ -4,16 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Yrll")
+		got := Hello("Yrll", "")
 		want := "Hello, Yrll"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say Hello in spanish when a second language parameter is included", func(t *testing.T) {
+		got := Hello("Yrll", "Spanish")
+		want := "Hola, Yrll"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say Hello in French", func(t *testing.T) {
+		got := Hello("Yrll", "French")
+		want := "Bonjour, Yrll"
 		assertCorrectMessage(t, got, want)
 	})
 }
